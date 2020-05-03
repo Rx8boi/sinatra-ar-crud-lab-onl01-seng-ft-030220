f058bf78993ecd87387d3067f3a04d8166a303ff
Charles Butler <cdbutle@outlook.com>
2020-05-03 21:23:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < Sinatra::Base
     @article = Article.all
     erb :index
   end
-  
+
   get '/articles/:id' do
     @article = Article.find(params[:id])
     erb :show
